6e2dbbea188b21282f73c35d4c2df85fd9e56323
XMUBQ <zbohan@umich.edu>
2024-03-31 22:31:33 -0400

    Delete preprocessing.py

--- preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-data_dir='/home/zbohan/dtr-text/data/coauthor-v1.0/'
-jss=os.listdir(data_dir)
-for jsf in jss:
-    df = pd.read_json(data_dir+jsf,lines=True)
-    print(df.eventName.value_counts())
-    break
